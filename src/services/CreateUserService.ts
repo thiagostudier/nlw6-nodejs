c17dbb0f33faca61fa5d0a6699ca8864fb1ef0fc
Thiago Studier <thiago.studier9@gmail.com>
2021-08-05 10:37:14 -0300

    AULA 2: CREATEUSERSERVICE

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean
+}
+
+class CreateUserService {
+  async execute({name, email, admin} : IUserRequest){
+    
+    const usersRepository = new UsersRepositories();
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if(userAlreadyExists){
+      throw new Error("User already exists");
+    }
+
+    const user = usersRepository.create({
+      name, 
+      email,
+      admin
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+
+  }
+}
+
+export { CreateUserService }
